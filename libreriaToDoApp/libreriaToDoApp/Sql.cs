652b9909bb598de96a4ad23caa87fc0c09d6b480
jonathan medina barrios <FTJonathanxx@hotmail.com>
2018-11-09 15:37:26 -0500

    Libreria de conexion a la base de datos

---
+++ libreriaToDoApp/libreriaToDoApp/Sql.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace libreriaToDoApp
+{
+    public class Sql
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+
+            NpgsqlConnection con = new NpgsqlConnection("Server=localhost; User Id=postgres;Password=proyecto;Database =todoapp; Integrated Security =True");
+            con.Open();
+            DataSet DS = new DataSet();
+            NpgsqlDataAdapter DP = new NpgsqlDataAdapter(cmd, con);
+            DP.Fill(DS);
+            con.Close();
+            return DS;
+        }
+    }
+}
