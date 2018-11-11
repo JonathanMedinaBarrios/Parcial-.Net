27c73ed27970b839550971a60695a73f8d67eafc
jonathan medina barrios <FTJonathanxx@hotmail.com>
2018-11-10 22:50:55 -0500

    se modificó la vista principal y se agregó la funcionalidad de almacenar los grupos en la base de datos y que esto se impriman al momento de iniciar sesión ( aun no se imprime despendiendo de quien es el padre del grupo)

--- libreriaToDoApp/ToDoApp/Principal.Designer.cs
+++ libreriaToDoApp/ToDoApp/Principal.Designer.cs
@@ -31,19 +31,18 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.tvGrupos = new System.Windows.Forms.TreeView();
-            this.gvTareas = new System.Windows.Forms.DataGridView();
-            this.btnAgregarFila = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnAgregarNodo = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gvTareas = new System.Windows.Forms.DataGridView();
             this.ColumnIDTarea = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btnAgregarNodo = new System.Windows.Forms.Button();
+            this.btnAgregarFila = new System.Windows.Forms.Button();
+            this.tvGrupos = new System.Windows.Forms.TreeView();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -51,7 +50,6 @@
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gvTareas)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -84,10 +82,9 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.Color.LightGray;
-            this.splitContainer1.Panel1.Controls.Add(this.button2);
-            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
-            this.splitContainer1.Panel1.Controls.Add(this.btnAgregarNodo);
             this.splitContainer1.Panel1.Controls.Add(this.tvGrupos);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAgregarNodo);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
             // 
             // splitContainer1.Panel2
             // 
@@ -98,14 +95,34 @@
             this.splitContainer1.SplitterDistance = 204;
             this.splitContainer1.TabIndex = 0;
             // 
-            // tvGrupos
+            // button2
             // 
-            this.tvGrupos.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.tvGrupos.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tvGrupos.Location = new System.Drawing.Point(0, 0);
-            this.tvGrupos.Name = "tvGrupos";
-            this.tvGrupos.Size = new System.Drawing.Size(204, 402);
-            this.tvGrupos.TabIndex = 0;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Image = global::ToDoApp.Properties.Resources.eliminar_2;
+            this.button2.Location = new System.Drawing.Point(50, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 36);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnAgregarNodo
+            // 
+            this.btnAgregarNodo.Image = global::ToDoApp.Properties.Resources.new_add_insert_file_13948;
+            this.btnAgregarNodo.Location = new System.Drawing.Point(5, 3);
+            this.btnAgregarNodo.Name = "btnAgregarNodo";
+            this.btnAgregarNodo.Size = new System.Drawing.Size(39, 36);
+            this.btnAgregarNodo.TabIndex = 0;
+            this.btnAgregarNodo.UseVisualStyleBackColor = true;
+            this.btnAgregarNodo.Click += new System.EventHandler(this.btnAgregarNodo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Remover tarea";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // gvTareas
             // 
@@ -119,39 +136,11 @@
             this.Column4,
             this.Column5,
             this.Column6});
-            this.gvTareas.Location = new System.Drawing.Point(-1, 45);
+            this.gvTareas.Location = new System.Drawing.Point(16, 45);
             this.gvTareas.Name = "gvTareas";
             this.gvTareas.ReadOnly = true;
-            this.gvTareas.Size = new System.Drawing.Size(628, 329);
+            this.gvTareas.Size = new System.Drawing.Size(645, 352);
             this.gvTareas.TabIndex = 0;
-            // 
-            // btnAgregarFila
-            // 
-            this.btnAgregarFila.Location = new System.Drawing.Point(21, 16);
-            this.btnAgregarFila.Name = "btnAgregarFila";
-            this.btnAgregarFila.Size = new System.Drawing.Size(95, 23);
-            this.btnAgregarFila.TabIndex = 1;
-            this.btnAgregarFila.Text = "Agregar tarea";
-            this.btnAgregarFila.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(150, 16);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Remover tarea";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 45);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(198, 354);
-            this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // ColumnIDTarea
             // 
@@ -189,24 +178,22 @@
             this.Column6.Name = "Column6";
             this.Column6.ReadOnly = true;
             // 
-            // button2
+            // btnAgregarFila
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Image = global::ToDoApp.Properties.Resources.eliminar_2;
-            this.button2.Location = new System.Drawing.Point(73, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(37, 36);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnAgregarFila.Location = new System.Drawing.Point(21, 16);
+            this.btnAgregarFila.Name = "btnAgregarFila";
+            this.btnAgregarFila.Size = new System.Drawing.Size(95, 23);
+            this.btnAgregarFila.TabIndex = 1;
+            this.btnAgregarFila.Text = "Agregar tarea";
+            this.btnAgregarFila.UseVisualStyleBackColor = true;
+            this.btnAgregarFila.Click += new System.EventHandler(this.btnAgregarFila_Click);
             // 
-            // btnAgregarNodo
+            // tvGrupos
             // 
-            this.btnAgregarNodo.Image = global::ToDoApp.Properties.Resources.new_add_insert_file_13948;
-            this.btnAgregarNodo.Location = new System.Drawing.Point(5, 3);
-            this.btnAgregarNodo.Name = "btnAgregarNodo";
-            this.btnAgregarNodo.Size = new System.Drawing.Size(39, 36);
-            this.btnAgregarNodo.TabIndex = 0;
-            this.btnAgregarNodo.UseVisualStyleBackColor = true;
+            this.tvGrupos.Location = new System.Drawing.Point(5, 45);
+            this.tvGrupos.Name = "tvGrupos";
+            this.tvGrupos.Size = new System.Drawing.Size(198, 352);
+            this.tvGrupos.TabIndex = 3;
             // 
             // Principal
             // 
@@ -223,7 +210,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gvTareas)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -234,10 +220,8 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button btnAgregarNodo;
-        private System.Windows.Forms.TreeView tvGrupos;
         private System.Windows.Forms.DataGridView gvTareas;
         private System.Windows.Forms.Button btnAgregarFila;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIDTarea;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNombre;
@@ -246,5 +230,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView tvGrupos;
     }
 }
--- libreriaToDoApp/ToDoApp/Principal.cs
+++ libreriaToDoApp/ToDoApp/Principal.cs
@@ -8,15 +8,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using libreriaToDoApp.Modelo;
+using libreriaToDoApp;
 
 namespace ToDoApp
 {
     public partial class Principal : Form
     {
+
+        public DataSet  data = null ;
+        string CMD; 
+
         public Principal()
         {
             InitializeComponent();
+            imprimirGrupo(1);
+            tvGrupos.ExpandAll();
         }
+
+
 
         private void btnAgregarNodo_Click(object sender, EventArgs e)
         {
@@ -25,29 +34,68 @@ namespace ToDoApp
 
 
             TreeNode nodoSeleccionado = this.tvGrupos.SelectedNode;
-            frm.agregarNodoPadre(""+this.tvGrupos.SelectedNode); 
+            frm.agregarNodoPadre("" + this.tvGrupos.SelectedNode);
             if (resultado == DialogResult.Yes)
             {
-                Grupo grupo = frm.GetGrupo(); 
+                Grupo grupo = frm.GetGrupo();
                 TreeNode nodo = new TreeNode();
                 nodo.Text = grupo.Nombre;
                 nodo.Tag = grupo;
 
+
                 if (nodoSeleccionado != null)
                 {
+                    CMD = "SELECT * FROM Grupos where nombre='"+ nodoSeleccionado.Text+"'";
+                    data = Sql.Ejecutar(CMD);
+                    int id_grupo = int.Parse(data.Tables[0].Rows[0]["Id_Grupo"].ToString().Trim());
+                    CMD = string.Format("INSERT INTO Grupos(Nombre, Id_Usuario,Id_GrupoPadre)" +
+                                                   "VALUES ('" + nodo.Text + "'," + 1 +","+id_grupo + ")");
+                    guardarNodo(CMD);
                     nodoSeleccionado.Nodes.Add(nodo);
+                    this.tvGrupos.SelectedNode = null; 
                 }
                 else
                 {
                     //Raiz
                     this.tvGrupos.Nodes.Add(nodo);
+                    CMD = string.Format("INSERT INTO Grupos(Nombre, Id_Usuario)" +
+                                                   "VALUES ('" + nodo.Text + "'," + 1 + ")");
+                    guardarNodo(CMD);
                 }
             }
+
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void btnAgregarFila_Click(object sender, EventArgs e)
         {
 
+        }
+
+
+        private void guardarNodo(string cmd) {
+            try{
+                 Sql.Ejecutar(cmd);
+            }
+            catch (Exception exc){
+                MessageBox.Show("Error al guardar el grupo en la base de datos..." + exc);
+                String error = "" + exc;
+            }
+         }
+
+        private void imprimirGrupo(int id){
+
+            CMD = "SELECT * FROM Grupos where Id_Usuario="+ id;
+            data = Sql.Ejecutar(CMD);
+
+            DataView dataViewHijos = new DataView(data.Tables[0]);
+
+            foreach (DataRowView dataRowCurrent in dataViewHijos)
+            {
+                TreeNode nodo = new TreeNode();
+                nodo.Text = dataRowCurrent["nombre"].ToString().Trim();
+                this.tvGrupos.Nodes.Add(nodo);
+            }
+                
         }
     }
 }
